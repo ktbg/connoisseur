08720cc58b24b2259196092413a6eb46c0d80de6
ktbg <ktboates@me.com>
2021-08-30 11:50:32 -0400

    initial api call test and rough js structure

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("js connected");
+async function getSearchItems (search){
+  try{
+    let res = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${search}`);
+    let searchResultIds = res.data.objectIDs;
+    let totalResults = res.data.total;
+  
+
+  } catch(error){
+    console.log(error);
+  }
+}
+
+// for testing, remove this later
+getSearchItems("flower");
