0a03a2293194fdcd9b8cc8c5571d66871f040fd0
ktbg <ktboates@me.com>
2021-08-31 11:48:33 -0400

    working on second api call for item details

--- script.js
+++ script.js
@@ -1,70 +1,97 @@
 // variables
-const searchUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=';
-const detailsUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+const searchUrl     = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=';
+const detailsUrl    = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+const artworkDetails  = {};
+// const details         = [];
+
+
 
 // get object IDs from search word or term
 async function getSearchItems(search){
   try{
     let res = await axios.get(`${searchUrl}${search}`);
-    // console.log(res.data.objectIDs);
-    // need to check if item is open access and if so add to array
-    // create function for that
-        let searchResultIds = res.data.objectIDs;
-        let totalResults = Object.keys(searchResultIds).length;
-        console.log(totalResults);
-    // console.log(totalResults);
-    // getDetails(searchResultIds);  - this function currently takes a number
+    let searchResultIds = res.data.objectIDs;
+    // let searchItemDetails = searchResultIds.map(
+    // console.log(searchItemDetails);
+
   } catch(error){
     console.log(error);
   }
 }
 
 // for testing, remove this later
-getSearchItems("claude%20monet");
+// getSearchItems("claude%20monet");
 
+// ****************************************************************************
 // get details for cards based on returned object ID array from getSearchItems
-async function getDetails(artworkId){
+// ****************************************************************************
+function getDetails(artworkId){
   try{
-    let res = await axios.get(`${detailsUrl}${artworkId}`)
-    let searchDetails = res.data;
-    console.log(searchDetails);
-    openAccess(searchDetails);
-    // MVP need: title, artistDisplayName
-    // need to check if item is open access and if so add to array
-    // creat function for that
-    let title = res.data.title;
-    console.log(title);
-    let artistDisplayName = res.data.artistDisplayName;
-    console.log(artistDisplayName);
+      return res = axios.get(`${detailsUrl}${artworkId}`)
+      // returns object of details
+      // let searchDetails = res.data;
+      // console.log(searchDetails);
+      // MVP need: title, artistDisplayName, img --> these are working
+      // ************ below kind of works, need to do it with an obj.
+      // details.push(res.data.title, res.data.artistDisplayName, res.data.primaryImage);
+      // console.log(details);
+      // let title = res.data.title;
+      // artworkDetails.title = title;
+      // let artistDisplayName = res.data.artistDisplayName;
+      // artworkDetails.artist = artistDisplayName;
+      // let artImg = res.data.primaryImage;
+      // artworkDetails.img = artImg;
+
+      // // console.log(artworkDetails);
+      // return artworkDetails;
+
+
     // postMVP need: medium, dimensions, objectDate, if!objectDate - objectBeginDate - objectEndDate, creditLine
-    // console.log(res);
   } catch(error){
     console.log(error);
   }
 }
 
+(async () => {
+  console.log(await(getDetails('436965')));
+});
+
 // getDetails('436965');
 
-// check if item is in public domain
-function openAccess(obj){
-  console.log(`original array length is ${Object.keys(obj).length}`);
-    let viewableItems = 0;
-  for (const item in obj) {
-    if(obj.isPublicDomain === true){
-      // let viewableItems = Map.prototype.set(`${item.key}`, `${item.value}`);
-      viewableItems += 1;
-    }
-  }
-  console.log(`acceptable array length is ${viewableItems}`);
-}
-// loop over objectID array and call getDetails to render details to the page
+// ****************************************************************************
+//  create cards based on number of ObjectIDS returned
+// ****************************************************************************
 
-// create cards based on number of ObjectIDS returned
-// create div with art-card class
+function renderDetails(obj){
+  // create div with art-card class, append to .results-grid
 // img tag with returned URL, alt text needs to be artwork title
 // h3 = art work title
 // p = artist
+// ^^ append to newly created div
+
+}
+
 // POSTMVP
 //    YEAR
 //    MEDIUM
 //    CREDIT
+
+// *************************************************************************************
+//              OPEN ACCESS CHECK - MAY ALREADY BE REMOVED (YET TO SEE THEM)
+// *************************************************************************************
+// check if item is in public domain
+// function openAccess(obj){
+//   console.log(`original array length is ${Object.keys(obj).length}`);
+//     let viewableItems = 0;
+//   for (const item in obj) {
+//     if(obj.isPublicDomain === true){
+//       // let viewableItems = Map.prototype.set(`${item.key}`, `${item.value}`);
+//       viewableItems += 1;
+//     }
+//   }
+//   console.log(`acceptable array length is ${viewableItems}`);
+// }
+
+// function renderDetails(){
+//   // loop over objectID array and call getDetails to render details to the page
+// }
