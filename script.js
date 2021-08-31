c0d71bb7a2f941575fa05235f490836b727ed878
ktbg <ktboates@me.com>
2021-08-31 15:05:53 -0400

    add renderDetails function

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ async function getSearchItems(search){
   try{
     let res = await axios.get(`${searchUrl}${search}`);
     let searchResultIds = res.data.objectIDs;
-    // let searchItemDetails = searchResultIds.map(
-    // console.log(searchResultIds);
     secondApi(searchResultIds);
+
+    // call renderDetails here
   } catch(error){
     console.log(error);
   }
@@ -22,64 +22,52 @@ async function getSearchItems(search){
 // for testing, remove this later
 getSearchItems("claude%20monet");
 
-// function getIdDetails(id){
-//   return secondApi(id);
-// }
-
-
-// ****************************************************************************
 // get details for cards based on returned object ID array from getSearchItems
-// ****************************************************************************
 function getDetails(artworkId){
   try{
       return axios.get(`${detailsUrl}${artworkId}`);
-      // returns object of details
-      // let searchDetails = res.data;
-      // console.log(searchDetails);
-      // MVP need: title, artistDisplayName, img --> these are working
-      // ************ below kind of works, need to do it with an obj.
-      // details.push(res.data.title, res.data.artistDisplayName, res.data.primaryImage);
-      // console.log(details);
-      // let title = res.data.title;
-      // artworkDetails.title = title;
-      // let artistDisplayName = res.data.artistDisplayName;
-      // artworkDetails.artist = artistDisplayName;
-      // let artImg = res.data.primaryImage;
-      // artworkDetails.img = artImg;
-
-      // // console.log(artworkDetails);
-      // return artworkDetails;
-
-
-    // postMVP need: medium, dimensions, objectDate, if!objectDate - objectBeginDate - objectEndDate, creditLine
   } catch(error){
     console.log(error);
   }
 }
 
-
+// async-await function for details based on objectId array returned from getSearchItems
+let searchDetails = [];
 async function secondApi(arr){
   for(let i = 0; i < arr.length; i++){
     const id = arr[i];
-    const searchDetails = await getDetails(id);
-    console.log(searchDetails);
+    const result = await getDetails(id);
+    searchDetails.push(result);
   }
+  console.log(searchDetails);
+  return(searchDetails);
 };
-// secondApi('436965');
-
-// secondApi('436965');
 
 // ****************************************************************************
 //  create cards based on number of ObjectIDS returned
 // ****************************************************************************
+const resultsGrid = document.querySelector(".results-grid");
+function renderDetails(arr){
+  resultsGrid.innerHTML = "";
+  arr.forEach((item) => {
+    // create div with art-card class, append to .results-grid
+    let newDiv = document.createElement("div");
+    newDiv.classList = "art-card";
+    // img tag with returned URL, alt text needs to be artwork title
+    let img = document.createElement("img");
+    img.setAttribute("src", item.primaryImage);
+    img.setAttribute("alt", item.title);
+    // h3 = art work title
+    let h3 = document.createElement("h3");
+    h3.innerText = `${item.title}`;
+    // p = artist
+    let p = document.createElement("p");
+    p.innerText = `${item.artistDisplayName}`
 
-function renderDetails(obj){
-  // create div with art-card class, append to .results-grid
-// img tag with returned URL, alt text needs to be artwork title
-// h3 = art work title
-// p = artist
+  })
+  resultsGrid.appendChild(newDiv);
+  newDiv.appendChild(img, h3, p);
 // ^^ append to newly created div
-
 }
 
 // POSTMVP
