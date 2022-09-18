953491935c1c5de0844b8ee56124b47ec2ec3cf9
ktbg <ktboates@me.com>
2022-09-18 16:40:16 -0400

    add css loader

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 // variables
-const searchUrl     = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=';
-const detailsUrl    = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
-const resultsGrid   = document.querySelector(".results-grid");
+const searchUrl =
+  "https://collectionapi.metmuseum.org/public/collection/v1/search?q=";
+const detailsUrl =
+  "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+const resultsGrid = document.querySelector(".results-grid");
 const searchSection = document.querySelector(".hero-search");
-const result        = document.querySelector(".result-total");
-const modal         = document.querySelector(".modal");
-const body          = document.querySelector("body");
+const result = document.querySelector(".result-total");
+const modal = document.querySelector(".modal");
+const body = document.querySelector("body");
+// ------------------------- css loader ---------------------
+const loader = document.querySelector("#loading");
 
-let input           = document.querySelector("#input");
-let searchDetails   = [];
-let startIndex      = 0;
-let endIndex        = 0;
-let modalContent    = document.querySelector(".modal-content");
+let input = document.querySelector("#input");
+let searchDetails = [];
+let startIndex = 0;
+let endIndex = 0;
+let modalContent = document.querySelector(".modal-content");
 
 // ----------------------------- on page load ---------------------------------------
 
+// css loader
+function displayLoading() {
+  loader.classList.add("display");
+}
+
+function hideLoading() {
+  loader.classList.remove("display");
+}
 // on page load, random works from the department code chosen
-async function onLoad(){
-  try{
-    let res = await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=painting');
+async function onLoad() {
+  // start css loader
+  displayLoading();
+  try {
+    let res = await axios.get(
+      "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=painting"
+    );
     let objectIds = res.data.objectIDs;
     startEnd(objectIds.length);
     const onLoadIds = objectIds.slice(startIndex, endIndex);
     secondApi(onLoadIds);
-  } catch(error){
+  } catch (error) {
     console.log(error);
   }
 }
@@ -31,7 +47,7 @@ async function onLoad(){
 onLoad();
 
 // find random starting index for objectIDs
-function startEnd(arrLength){
+function startEnd(arrLength) {
   startIndex = Math.floor(Math.random() * arrLength);
   endIndex = startIndex + 50;
 }
@@ -50,101 +66,101 @@ searchSection.addEventListener("submit", (e) => {
 });
 
 // get object IDs from search word or term
-async function getSearchItems(search){
-  try{
+async function getSearchItems(search) {
+  try {
     let res = await axios.get(`${searchUrl}${search}`);
     let searchResultIds = res.data.objectIDs;
     secondApi(searchResultIds);
-  } catch(error){
+  } catch (error) {
     console.log(error);
   }
 }
 
 // get details for cards based on returned object ID array from getSearchItems
-function getDetails(artworkId){
-  try{
-      return axios.get(`${detailsUrl}${artworkId}`);
-  } catch(error){
+function getDetails(artworkId) {
+  try {
+    return axios.get(`${detailsUrl}${artworkId}`);
+  } catch (error) {
     console.log(error);
   }
 }
 
 // async-await function for details based on objectId array returned from getSearchItems
 // capped at 50 items due to load time
-async function secondApi(arr){
+async function secondApi(arr) {
   let qtyReturned = 0;
-  if(arr.length > 30){
+  if (arr.length > 30) {
     qtyReturned = 30;
   } else {
     qtyReturned = arr.length;
   }
-  for(let i = 0; i < qtyReturned; i++){
+  for (let i = 0; i < qtyReturned; i++) {
     const id = arr[i];
     const result = await getDetails(id);
     searchDetails.push(result);
   }
   renderDetails(searchDetails);
-};
+}
 
 // ------------------------------- DOM append ---------------------------------------
-function renderDetails(arr){
+function renderDetails(arr) {
   arr.forEach((item) => {
+    // ======================= create art-card ==================================
 
-      // ======================= create art-card ==================================
+    let newDiv = document.createElement("div");
+    newDiv.classList = "art-card";
+    newDiv.setAttribute("name", `${item.data.objectID}`);
+    // event listener for modal click functionality
+    newDiv.addEventListener("click", (e) => {
+      modalInfo(e.target.name);
+    });
 
-      let newDiv = document.createElement("div");
-      newDiv.classList = "art-card";
-      newDiv.setAttribute("name", `${item.data.objectID}`);
-          // event listener for modal click functionality
-      newDiv.addEventListener("click", (e) => {
-        modalInfo(e.target.name);
-      });
+    // ======================= img div and image ==================================
 
-      // ======================= img div and image ==================================
+    let imgDiv = document.createElement("div");
+    imgDiv.classList = "imgDiv";
+    imgDiv.setAttribute("name", `${item.data.objectID}`);
+    // img tag with returned URL, alt text needs to be artwork title
+    let img = document.createElement("img");
+    img.classList = "result-img";
+    img.setAttribute("src", item.data.primaryImage);
+    img.setAttribute("alt", item.data.title);
+    img.setAttribute("name", `${item.data.objectID}`);
 
-      let imgDiv = document.createElement("div");
-      imgDiv.classList = "imgDiv";
-      imgDiv.setAttribute("name", `${item.data.objectID}`);
-          // img tag with returned URL, alt text needs to be artwork title
-        let img = document.createElement("img");
-        img.classList = "result-img";
-        img.setAttribute("src", item.data.primaryImage);
-        img.setAttribute("alt", item.data.title);
-        img.setAttribute("name", `${item.data.objectID}`);
+    // ======================= text div and text ==================================
 
-      // ======================= text div and text ================================== 
-          
-      let textDiv = document.createElement("div");
-      textDiv.classList = "textDiv";
-      textDiv.setAttribute("name", `${item.data.objectID}`);
-          // h4 artwork title
-      let h4 = document.createElement("h4");
-      h4.innerText = `${item.data.title}`;
-      h4.setAttribute("name", `${item.data.objectID}`);
-          // p = artist
-      let p = document.createElement("p");
-      p.innerHTML = `<strong>${item.data.artistDisplayName}</strong>`;
-      p.setAttribute("name", `${item.data.objectID}`);
+    let textDiv = document.createElement("div");
+    textDiv.classList = "textDiv";
+    textDiv.setAttribute("name", `${item.data.objectID}`);
+    // h4 artwork title
+    let h4 = document.createElement("h4");
+    h4.innerText = `${item.data.title}`;
+    h4.setAttribute("name", `${item.data.objectID}`);
+    // p = artist
+    let p = document.createElement("p");
+    p.innerHTML = `<strong>${item.data.artistDisplayName}</strong>`;
+    p.setAttribute("name", `${item.data.objectID}`);
 
-      // ================ append newly created items to respective div =============== 
+    // ================ append newly created items to respective div ===============
 
-          // ^^ append to newly created div
-      resultsGrid.appendChild(newDiv);
-      newDiv.append(imgDiv, textDiv);
-      imgDiv.appendChild(img);
-      textDiv.append(h4, p);
-  })
+    // ^^ append to newly created div
+    resultsGrid.appendChild(newDiv);
+    newDiv.append(imgDiv, textDiv);
+    imgDiv.appendChild(img);
+    textDiv.append(h4, p);
+  });
+  hideLoading();
 }
 
 // -------------------------------- modal ------------------------------------------
 // gets additional modal info, medium, dimensions, creditLine
-function modalInfo(itemId){
+function modalInfo(itemId) {
   getModalDetails(itemId);
 }
 
-async function getModalDetails(artworkId){
+async function getModalDetails(artworkId) {
   // get details for this item
-  try{
+  try {
     let res = await axios.get(`${detailsUrl}${artworkId}`);
     const modalImg = ["image", res.data.primaryImage];
     const modalArtist = ["artist", `${res.data.artistDisplayName}`];
@@ -155,67 +171,81 @@ async function getModalDetails(artworkId){
     const modalMedium = ["medium", `${res.data.medium}`];
     const modalDimensions = ["dimensions", `${res.data.dimensions}`];
     const modalCredit = ["credit", `${res.data.creditLine}`];
-    const map = new Map([modalImg, modalArtist, modalTitle, modalDate, modalBeginDate, modalEndDate, modalMedium,  modalDimensions, modalCredit])
+    const map = new Map([
+      modalImg,
+      modalArtist,
+      modalTitle,
+      modalDate,
+      modalBeginDate,
+      modalEndDate,
+      modalMedium,
+      modalDimensions,
+      modalCredit,
+    ]);
     const modalInfo = Object.fromEntries(map);
     // render modal details with item information returned above
     renderModal(modalInfo);
-  }catch(error){
+  } catch (error) {
     console.log(error);
   }
 }
 // idea for modal code from: https://www.w3schools.com/howto/howto_css_modals.asp
 // ------------------------------- modal html elements --------------------------------------
-function renderModal(obj){
-    // clear modal-content section
+function renderModal(obj) {
+  // clear modal-content section
   modalContent.innerHTML = "";
-    // ============================= span to close modal ===========================
-    // add click listener to modal span to close modal
+  // ============================= span to close modal ===========================
+  // add click listener to modal span to close modal
   let span = document.querySelector(".close");
   span.addEventListener("click", () => {
     modal.style.display = "none";
     modal.style.top = 0;
-  })
-    // ============================= img div and image ==============================
-  let imgDiv        = document.createElement("div");
-  imgDiv.classList  = "imgDiv";
-      //// img tag with returned URL, alt text needs to be artwork title
-  let img           = document.createElement("img");
-  img.classList     = "modal-img";
+  });
+  // ============================= img div and image ==============================
+  let imgDiv = document.createElement("div");
+  imgDiv.classList = "imgDiv";
+  //// img tag with returned URL, alt text needs to be artwork title
+  let img = document.createElement("img");
+  img.classList = "modal-img";
   img.setAttribute("src", obj.image);
   img.setAttribute("alt", `${obj.title}`);
-    // ============================== text div and all text =========================
+  // ============================== text div and all text =========================
   let textDiv = document.createElement("div");
   textDiv.classList = "textDiv";
-      // h3 = art work title
+  // h3 = art work title
   let h4 = document.createElement("h4");
   h4.innerText = `${obj.title}`;
-      // p = artist
+  // p = artist
   let p = document.createElement("p");
   p.innerHTML = `<strong>${obj.artist}</strong>`;
-      // ul to contain addition information
-  let ul = document.createElement("ul"); 
+  // ul to contain addition information
+  let ul = document.createElement("ul");
   let liYear = document.createElement("li");
-      // some works don't have a date so date range of artist is returned
-    if(obj.objectDate === ""){
-      liYear.innerHTML = `<strong>Year:</strong> ${obj.objectBeginDate} - ${obj.objectEndDate}`;
-    } else {
-      liYear.innerHTML = `<strong>Year:</strong> ${obj.objectDate}`;
-    }
-    let liMedium = document.createElement("li");
-    liMedium.innerHTML = `<strong>Medium:</strong> ${obj.medium}`;
-    let liDimensions = document.createElement("li");
-    liDimensions.innerHTML = `<strong>Dimensions:</strong> ${obj.dimensions}`;
-    let liCredit = document.createElement("li");
-    liCredit.innerHTML = `<strong>Credit:</strong> ${obj.credit}`;
-    //=============================== append items to respective div ================
+  // some works don't have a date so date range of artist is returned
+  if (obj.objectDate === "") {
+    liYear.innerHTML = `<strong>Year:</strong> ${obj.objectBeginDate} - ${obj.objectEndDate}`;
+  } else {
+    liYear.innerHTML = `<strong>Year:</strong> ${obj.objectDate}`;
+  }
+  let liMedium = document.createElement("li");
+  liMedium.innerHTML = `<strong>Medium:</strong> ${obj.medium}`;
+  let liDimensions = document.createElement("li");
+  liDimensions.innerHTML = `<strong>Dimensions:</strong> ${obj.dimensions}`;
+  let liCredit = document.createElement("li");
+  liCredit.innerHTML = `<strong>Credit:</strong> ${obj.credit}`;
+  //=============================== append items to respective div ================
   modalContent.append(imgDiv, textDiv);
   imgDiv.appendChild(img);
   textDiv.append(h4, p, ul);
   ul.append(liYear, liMedium, liDimensions, liCredit);
-    // ============================= display modal on screen ========================
-      // display modal in proper viewport for desktop versus mobile
-      // if logic code idea from: https://dev.to/timhuang/a-simple-way-to-detect-if-browser-is-on-a-mobile-device-with-javascript-44j3
-  if(/Android|webOs|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // ============================= display modal on screen ========================
+  // display modal in proper viewport for desktop versus mobile
+  // if logic code idea from: https://dev.to/timhuang/a-simple-way-to-detect-if-browser-is-on-a-mobile-device-with-javascript-44j3
+  if (
+    /Android|webOs|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
     modal.style.top = 0;
   } else {
     // viewPort logic mine from MDN research
@@ -224,4 +254,3 @@ function renderModal(obj){
   }
   modal.style.display = "block";
 }
-
